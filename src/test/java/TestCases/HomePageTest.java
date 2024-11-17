9856107a2be3f33fc8c7aa0c29cc37027ff99017
Subin <subinsb135@gmail.com>
2024-11-17 21:41:15 +0530

    new commit

--- src/test/java/TestCases/HomePageTest.java
+++ src/test/java/TestCases/HomePageTest.java
@@ -4,16 +4,17 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import PageObjects.HomePage;
+import PageObjects.LoginPage;
 
 public class HomePageTest extends TestBase{
-	HomePage HP;
+	HomePage hp;
 
-	LoginPage LP;
+	LoginPage lp;
   
 	@Test (priority=1)
 	public void TC_HP_7() {
-		HP=new HomePage(driver);
-		boolean P= HP.loginbtnpresent();
+		hp=new HomePage(driver);
+		boolean P= hp.loginbtnpresent();
 		Assert.assertTrue(P, "Login button is not present");
 		System.out.println("Login button present: " + P);
 	}
@@ -21,15 +22,15 @@ public class HomePageTest extends TestBase{
 
 	@Test(priority=2)
 	public void TC_HP_5() {
-		HP=new HomePage(driver);
-		boolean L=HP.logodisplayed();
+		hp=new HomePage(driver);
+		boolean L=hp.logodisplayed();
 		Assert.assertTrue(L, "Logo is not displayed");
 		System.out.println("Logo icon present: " + L);
 	}
 
 	@Test(priority=3)
 	public void TC_HP_6() {
-		String message = HP.welcomemsgPresent();
+		String message = hp.welcomemsgPresent();
 		Assert.assertEquals(message,"Welcome to ICT Academy of Kerala");
 		System.out.println("Welcome message is present: " + message);
         Assert.assertNotNull(message, "Welcome message is missing");
@@ -39,8 +40,8 @@ public class HomePageTest extends TestBase{
 
 	@Test(priority=4)
 	public void TC_HP_3() {
-		HP=new HomePage(driver);
-		boolean J = HP.joinusbtnEnable();
+		hp=new HomePage(driver);
+		boolean J = hp.joinusbtnEnable();
 		Assert.assertTrue(J, "Join us button is not present");
 		System.out.println("Join us button present: " + J);
 
@@ -49,20 +50,20 @@ public class HomePageTest extends TestBase{
 	@Test(priority=5)
 	public void TC_HP_8() {
 
-		HP=new HomePage(driver);
-		LP= new LoginPage(driver);
-		HP.loginbtnClick();
-		String url= LP.getUrlcurrent();
+		hp=new HomePage(driver);
+		lp= new LoginPage(driver);
+		hp.loginbtnClick();
+		String url= lp.getUrlcurrent();
 		Assert.assertEquals(url, "https://ictak-internship-portal-client.vercel.app/login");
 		System.out.println("directed to login page"); 
 
 	}
-}
-    HP=new HomePage(driver);
-    boolean J = HP.joinusbtnEnable();
-    Assert.assertTrue(J, "Join us button is not present");
-    System.out.println("Join us button present: " + J);
-  
-    }
+
+//    HP=new HomePage(driver);
+//    boolean J = hp.joinusbtnEnable();
+//    Assert.assertTrue(J, "Join us button is not present");
+//    System.out.println("Join us button present: " + J);
+//  
+//    }
     
 }
