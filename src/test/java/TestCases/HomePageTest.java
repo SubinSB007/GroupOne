d1deb4fe172d2a82ee071626a262e1bf75bd3271
Subin <subinsb135@gmail.com>
2024-11-21 12:29:44 +0530

    changed

--- src/test/java/TestCases/HomePageTest.java
+++ src/test/java/TestCases/HomePageTest.java
@@ -11,102 +11,52 @@ import PageObjects.LoginPage;
 
 public class HomePageTest extends TestBase{
 	HomePage hp;
-
-<<<<<<< HEAD
 	LoginPage lp;
   
-=======
 	LoginPage LP;
 	@BeforeClass
 	public void setUP() {
-		HP = new HomePage(driver);
+		hp = new HomePage(driver);
 		LP = new LoginPage(driver);
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
 	}
->>>>>>> branch 'Subin' of https://github.com/SubinSB007/GroupOne.git
+	
 	@Test (priority=1)
-<<<<<<< HEAD
-	public void TC_HP_7() {
-		hp=new HomePage(driver);
-		boolean P= hp.loginbtnpresent();
-=======
 	public void testLoginBtnPresent() {
-		boolean P= HP.loginbtnpresent();
->>>>>>> branch 'Subin' of https://github.com/SubinSB007/GroupOne.git
+		boolean P= hp.loginbtnpresent();
 		Assert.assertTrue(P, "Login button is not present");
 		
 	}
 
 
 	@Test(priority=2)
-<<<<<<< HEAD
-	public void TC_HP_5() {
-		hp=new HomePage(driver);
-		boolean L=hp.logodisplayed();
-=======
 	public void testLogoIsDisplayed() {
-		boolean L=HP.logodisplayed();
->>>>>>> branch 'Subin' of https://github.com/SubinSB007/GroupOne.git
+		boolean L=hp.logodisplayed();
 		Assert.assertTrue(L, "Logo is not displayed");
 		
 	}
 
 	@Test(priority=3)
-<<<<<<< HEAD
-	public void TC_HP_6() {
-		String message = hp.welcomemsgPresent();
-		Assert.assertEquals(message,"Welcome to ICT Academy of Kerala");
-		System.out.println("Welcome message is present: " + message);
-        Assert.assertNotNull(message, "Welcome message is missing");
-        System.out.println("Welcome message is present: " + message);
-=======
 	public void testWelcomeMessagepresent() {
-		String message = HP.welcomemsgPresent();
+		String message = hp.welcomemsgPresent();
 		Assert.assertEquals(message,"Welcome to ICT Academy of Kerala");     
->>>>>>> branch 'Subin' of https://github.com/SubinSB007/GroupOne.git
 	}
 
 
 	@Test(priority=4)
-<<<<<<< HEAD
-	public void TC_HP_3() {
-		hp=new HomePage(driver);
-		boolean J = hp.joinusbtnEnable();
-=======
 	public void testJoinUsBtnEnable() {
-		boolean J = HP.joinusbtnEnable();
->>>>>>> branch 'Subin' of https://github.com/SubinSB007/GroupOne.git
+		boolean J = hp.joinusbtnEnable();
 		Assert.assertTrue(J, "Join us button is not present");
 	}
 
 	@Test(priority=5)
-<<<<<<< HEAD
-	public void TC_HP_8() {
-
-		hp=new HomePage(driver);
-		lp= new LoginPage(driver);
-		hp.loginbtnClick();
-		String url= lp.getUrlcurrent();
-=======
 	public void testLoginBtnFunctionality() {
-		HP.loginbtnClick();
+		hp.loginbtnClick();
 		String url= LP.getUrlcurrent();
->>>>>>> branch 'Subin' of https://github.com/SubinSB007/GroupOne.git
 		Assert.assertEquals(url, "https://ictak-internship-portal-client.vercel.app/login");
-<<<<<<< HEAD
 		System.out.println("directed to login page"); 
 
 	}
-
-//    HP=new HomePage(driver);
-//    boolean J = hp.joinusbtnEnable();
-//    Assert.assertTrue(J, "Join us button is not present");
-//    System.out.println("Join us button present: " + J);
-//  
-//    }
-=======
-    }
->>>>>>> branch 'Subin' of https://github.com/SubinSB007/GroupOne.git
     
 }
--- src/test/java/TestCases/MentorList.java
+++ src/test/java/TestCases/MentorList.java
@@ -1,6 +1,101 @@
 package TestCases;
 
+import java.time.Duration;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import PageObjects.AdminDashboardPage;
+import PageObjects.HomePage;
+import PageObjects.LoginPage;
+import PageObjects.MentorListPage;
+import Utilities.DataproviderAddMentor;
+
 public class MentorList extends TestBase{
 	
+	HomePage hp;
+	LoginPage lp;
+	AdminDashboardPage adp;
+	MentorListPage mlp;
+	
+	@BeforeClass
+	public void setUp() {
+		hp=new HomePage(driver);
+		lp=new LoginPage(driver);
+		adp=new AdminDashboardPage(driver);
+		mlp= new MentorListPage(driver);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		hp.loginbtnClick();
+		lp.setEmail("ictak@example.com");
+		lp.setPass("0000");
+		lp.login();
+		}
+	
+	@Test(priority=1)
+	public void testMentorListPageNavigation() {
+		adp.mentorMenuItemClick();
+		Assert.assertEquals(mlp.currentUrlReturn(), "https://ictak-internship-portal-client.vercel.app/admin/mentorslist/");
+	}
+	
+	@Test(priority=2)
+	public void testMentorListHeader() {
+		Assert.assertEquals(mlp.mentorListheadMentorText(), "Mentors");
+		Assert.assertEquals(mlp.mentorListHeadEmailText(), "Email");
+		Assert.assertEquals(mlp.mentorListHeadphoneText(), "Phone");
+		Assert.assertEquals(mlp.mentorListHeadTopicsText(), "Project Topics");
+		Assert.assertEquals(mlp.mentorListHeadActionText(), "Actions");
+	}
+	
+	@Test(priority=3)
+	public void testAddmentorButtonVisible() {
+		Assert.assertTrue(mlp.addMentorbtnVisible());
+	}
+	
+	@Test(priority=4)
+	public void testAddMentorDialogBox() {
+		mlp.clickAddMentorButton();
+		Assert.assertTrue(mlp.checkNamefieldVisible());
+		Assert.assertTrue(mlp.checkEmailFieldVisible());
+		Assert.assertTrue(mlp.checkPasswordfieldvisible());
+		Assert.assertTrue(mlp.checkPhoneFieldVisible());
+		Assert.assertTrue(mlp.checkTopicFieldVisible());
+		mlp.clickCancelBtnAddDialogBox();
+	}
+	@Test(priority=5)
+	public void testAddedProjectTopicDropdown() throws InterruptedException {
+		mlp.clickDashboardSidebar();
+		adp.addProjectButtonClick();
+		adp.addProjectDialogTopicEnter(prop.getProperty("topictest"));
+		adp.addTopicDialogDurationEnter(prop.getProperty("durationtest"));
+		adp.clickAddbtnAddProjectDialog();
+		adp.mentorMenuItemClick();
+		mlp.clickAddMentorButton();
+		int f=mlp.checkAddedProjectTopicdropDown(prop.getProperty("topictest"));
+		mlp.clickCancelBtnAddDialogBox();
+		if(f==1) {
+			Assert.assertTrue(true);
+		}else {
+			Assert.fail();
+		}
+	
+	}
+	
+	@Test(priority=6,dataProvider = "testAddMentorValid", dataProviderClass = DataproviderAddMentor.class)
+	public void testValidDataEntryAddMentor(String name,String email,String phone,String password,String topic) throws InterruptedException {
+		mlp.clickAddMentorButton();
+		mlp.enterNameAddMentorDialog(name);
+		mlp.enterEmailAddMentorDialog(email);
+		mlp.enterPhoneFieldAddMentorDialog(phone);
+		mlp.enterPasswordAddMentorDialog(password);
+		mlp.enterTopicAddMentorDialog(topic);
+		mlp.clickAddbtnAddDialogBox();
+		int f=mlp.checkMentorIsAdded(name);
+		if(f==1) {
+			Assert.assertTrue(true);
+		}else {
+			Assert.fail();
+		}
+	}
 
 }
